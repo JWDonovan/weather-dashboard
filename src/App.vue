58fed325d98f047a740859b49fd964d1111821b2
James Donovan <jameswilliamdonovan@gmail.com>
2022-03-16 16:59:13 -0400

    added code to test axios openweathermap api call

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,27 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <button @click="fetchUsers">Fetch Users</button>
   </nav>
   <router-view />
 </template>
+
+<script lang="ts">
+import axios from "axios";
+
+export default {
+  name: "app",
+  methods: {
+    fetchUsers: function () {
+      const apiKey = process.env.VUE_APP_API_KEY;
+      const baseURI = `https://api.openweathermap.org/data/2.5/weather?lat=39.9612&lon=82.9988&appid=${apiKey}`;
+      axios.get(baseURI).then((result) => {
+        console.log(result.data);
+      });
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
