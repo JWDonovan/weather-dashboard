46f528564dda7cf779bc10f4207837bef855d371
James Donovan <jameswilliamdonovan@gmail.com>
2022-03-18 22:42:12 -0400

    updated main app component with main panel and side panel components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
-  <div></div>
+  <main class="main">
+    <SidePanel />
+    <MainPanel />
+  </main>
 </template>
 
 <script lang="ts">
+import SidePanel from "@/components/SidePanel.vue";
+import MainPanel from "@/components/MainPanel.vue";
+
 export default {
   name: "app",
+  components: {
+    SidePanel,
+    MainPanel,
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "@/styles/vars" as *;
+
+.main {
+  background-color: $blue;
+  display: flex;
+  min-height: 100vh;
+}
+</style>
