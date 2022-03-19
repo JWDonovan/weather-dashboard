62be8f5a6a13f44483292ec0134c4d999a7e6071
James Donovan <jameswilliamdonovan@gmail.com>
2022-03-18 22:40:31 -0400

    created component SidePanelHeader

---
+++ src/components/SidePanelHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="side-panel-header">
+    <NewLocationButton />
+    <TemperatureToggle />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import NewLocationButton from "./NewLocationButton.vue";
+import TemperatureToggle from "./TemperatureToggle.vue";
+
+export default defineComponent({
+  components: {
+    NewLocationButton,
+    TemperatureToggle,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@use "@/styles/vars" as *;
+
+.side-panel-header {
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
