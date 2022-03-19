bb8311193db1c633510a045b7565919918940e4d
James Donovan <jameswilliamdonovan@gmail.com>
2022-03-18 22:41:24 -0400

    created component SidePanel

---
+++ src/components/SidePanel.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="side-panel">
+    <SidePanelHeader />
+    <SidePanelDayInfo />
+    <SidePanelTemp />
+    <WeatherImage iconId="10d" />
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import SidePanelHeader from "./SidePanelHeader.vue";
+import SidePanelDayInfo from "./SidePanelDayInfo.vue";
+import SidePanelTemp from "./SidePanelTemp.vue";
+import WeatherImage from "./WeatherImage.vue";
+
+export default defineComponent({
+  components: {
+    SidePanelHeader,
+    SidePanelDayInfo,
+    SidePanelTemp,
+    WeatherImage,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@use "@/styles/vars" as *;
+
+.side-panel {
+  background-color: transparent;
+  box-sizing: border-box;
+  flex: none;
+  padding: 4em;
+  user-select: none;
+  width: 30em;
+}
+</style>
