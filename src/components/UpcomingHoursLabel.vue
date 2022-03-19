756da1a2d73efa80aaf5dcd099d22ae0667cbe90
James Donovan <jameswilliamdonovan@gmail.com>
2022-03-19 04:18:43 -0400

    created component UpcomingHoursLabel

---
+++ src/components/UpcomingHoursLabel.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="label">
+    <span class="time">Now</span>
+    <img
+      alt="weather icon"
+      class="img"
+      src="https://openweathermap.org/img/wn/10d.png"
+    />
+    <span class="temp">56°</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "@/styles/vars" as *;
+
+.label {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 0.25em;
+}
+
+.img {
+  background-color: rgba($blue, 0.3);
+  border-radius: 50%;
+  width: 2.5em;
+}
+</style>
