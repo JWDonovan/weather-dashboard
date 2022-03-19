92f2f546461bbe2f405a56d0e70eb64440ab0437
James Donovan <jameswilliamdonovan@gmail.com>
2022-03-18 22:40:58 -0400

    created component MainPanel

---
+++ src/components/MainPanel.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="main-panel"></section>
+</template>
+
+<style lang="scss" scoped>
+@use "@/styles/vars" as *;
+
+.main-panel {
+  background-color: $blue-light;
+  border-radius: $border-radius 0 0 $border-radius;
+  flex-grow: 1;
+}
+</style>
