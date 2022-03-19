d9db778b7f41baca75c4cd18ac2ef10f2768d25a
James Donovan <jameswilliamdonovan@gmail.com>
2022-03-18 21:46:56 -0400

    created WeatherImage component

---
+++ src/components/WeatherImage.vue
@@ -0,0 +1,25 @@
+<template>
+  <img
+    class="weather-icon"
+    alt="current weather icon"
+    v-bind:src="'http://openweathermap.org/img/wn/' + iconId + '@4x.png'"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "WeatherIcon",
+  props: ["iconId"],
+});
+</script>
+
+<style lang="scss" scoped>
+.weather-icon {
+  display: block;
+  margin: 2em auto;
+  max-width: 100%;
+  width: 100vw;
+}
+</style>
